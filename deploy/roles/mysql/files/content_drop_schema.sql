7d23eb904eb3a533611dd9243611c5860ced57da
Lee Gathercole <lee@fathomatics.com>
2015-06-09 09:27:52 +0100

    Include checks for tables to exist

--- deploy/roles/mysql/files/content_drop_schema.sql
+++ deploy/roles/mysql/files/content_drop_schema.sql
@@ -1,40 +1,78 @@
 
-    alter table OBJECT_DATA 
-        drop 
-        foreign key FK35EEB8AAF8AD2AC8;
+    IF EXISTS(SELECT table_name 
+            FROM INFORMATION_SCHEMA.TABLES
+           WHERE table_schema = 'content'
+             AND table_name LIKE 'OBJECT_DATA')
 
-    alter table OBJECT_DATA 
-        drop 
-        foreign key FK35EEB8AA1FD3CA09;
+    THEN
+        alter table OBJECT_DATA 
+            drop 
+            foreign key FK35EEB8AAF8AD2AC8;
 
-    alter table OBJECT_DATA 
-        drop 
-        foreign key FK35EEB8AA23EE9F15;
+        alter table OBJECT_DATA 
+            drop 
+            foreign key FK35EEB8AA1FD3CA09;
 
-    alter table OBJECT_TYPE_DEFINITION 
-        drop 
-        foreign key FK84B3967847D86058;
+        alter table OBJECT_DATA 
+            drop 
+            foreign key FK35EEB8AA23EE9F15;
+    END IF;
 
-    alter table PROPERTY_DATA 
-        drop 
-        foreign key FK7EB72FD477E6A2E1;
+    IF EXISTS(SELECT table_name 
+            FROM INFORMATION_SCHEMA.TABLES
+           WHERE table_schema = 'content'
+             AND table_name LIKE 'OBJECT_TYPE_DEFINITION')
 
-    alter table PROPERTY_DATA 
-        drop 
-        foreign key FK7EB72FD45DA1F315;
+    THEN
+        alter table OBJECT_TYPE_DEFINITION 
+            drop 
+            foreign key FK84B3967847D86058;
+    END IF;
 
-    alter table PROPERTY_DEFINITION 
-        drop 
-        foreign key FK403A21FDF8AD2AC8;
+    IF EXISTS(SELECT table_name 
+            FROM INFORMATION_SCHEMA.TABLES
+           WHERE table_schema = 'content'
+             AND table_name LIKE 'PROPERTY_DATA')
 
-    alter table RENDITION 
-        drop 
-        foreign key FK13F83D541FD3CA09;
+    THEN
+    
+        alter table PROPERTY_DATA 
+            drop 
+            foreign key FK7EB72FD477E6A2E1;
 
-    alter table RENDITION 
-        drop 
-        foreign key FK13F83D545DA1F315;
+        alter table PROPERTY_DATA 
+            drop 
+            foreign key FK7EB72FD45DA1F315;
+    END IF;
 
+    IF EXISTS(SELECT table_name 
+            FROM INFORMATION_SCHEMA.TABLES
+           WHERE table_schema = 'content'
+             AND table_name LIKE 'PROPERTY_DEFINITION')
+
+    THEN
+    
+        alter table PROPERTY_DEFINITION 
+            drop 
+            foreign key FK403A21FDF8AD2AC8;
+    END IF;
+
+    IF EXISTS(SELECT table_name 
+            FROM INFORMATION_SCHEMA.TABLES
+           WHERE table_schema = 'content'
+             AND table_name LIKE 'RENDITION')
+
+    THEN
+    
+        alter table RENDITION 
+            drop 
+            foreign key FK13F83D541FD3CA09;
+
+        alter table RENDITION 
+            drop 
+            foreign key FK13F83D545DA1F315;
+    END IF;
+    
     drop table if exists CONTENT_STREAM;
 
     drop table if exists OBJECT_DATA;
